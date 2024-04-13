c8dd14560a5904435852ab55cec516cc8b700b44
luoyu123321 <779231460@qq.com>
2024-04-14 03:36:30 +0800

    页面操作实时更新数据库测试

--- app/page.tsx
+++ app/page.tsx
@@ -1,42 +1,48 @@
 "use client";
 import prisma from '@/prisma';
 // import Request from '@/app/api/register/route'; 
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 export default function Home() {
-  // GET().then((res)=>{
-  //   console.log(111,res);
-  //   data = res;
-  // })
-  useEffect( () => {
-    const test = {title:'laorytest3',content:'laorytest3content'};
-    const params = JSON.stringify(test);
-    const aaa =()=> {
-      fetch('/api/register', {
-        method: 'GET',
-        headers: {
-          'Content-Type': 'application/json',
-        }
-      })
-      .then((res)=>res.json())
-      .then((res)=>{
-        console.log('res',res);
-      })
-      // fetch('/api/register', {
-      //   method: 'POST',
-      //   headers: {
-      //     'Content-Type': 'application/json',
-      //   },
-      //   body: params,
-      // })
-      // .then((res)=>{
-      //   console.log('res',res);
-      // })
-    }
-    aaa();
+  const [numbers, setNumbers] = useState<number>(3);
+  const [dataSourse, setDataSourse] = useState<{ title: string, content: string }[]>([]);
+  useEffect(() => {
+    getData();
   }, []);
+
+  const getData =  () => {
+    fetch('/api/register', {
+      method: 'GET',
+      headers: {
+        'Content-Type': 'application/json',
+      }
+    })
+      .then((res) => res.json())
+      .then(({post}) => {
+        setDataSourse(post);
+        console.log('post', post);
+      })
+  }
+
+  const Btn = () => {
+    fetch('/api/register', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({ title: 'laorytest' + numbers, content: 'laorytest' + numbers + 'content' }),
+    })
+      .then((res) => {
+        console.log('res', res);
+        setNumbers(numbers + 1);
+        getData();
+      })
+  }
   return (
     <>
-      <div>hello prisma</div>
+      <button onClick={Btn} >加一</button>
+      {dataSourse.map((item, index) => {
+        return <div key={index}>{item.title+'------'+item.content}</div>
+      })}
     </>
   )
 }
